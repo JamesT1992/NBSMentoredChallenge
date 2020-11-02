6e0ea19e43582227078bf6e1a74a722b69825612
JamesT1992 <james.trinder@nationwide.co.uk>
2020-11-02 16:45:35 +0000

    final commit for the day

--- scripts/sonarqube.sh
+++ scripts/sonarqube.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-sonar-scanner \
+echo "sonar-scanner \
   -Dsonar.projectKey=first-project \
   -Dsonar.sources=. \
   -Dsonar.host.url=http://localhost:9000 \
-  -Dsonar.login=aa0e08500d505b5362ff994999cf892f19bf7ea0
+  -Dsonar.login=aa0e08500d505b5362ff994999cf892f19bf7ea0"
 
