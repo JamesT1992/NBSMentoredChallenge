07a09f84af94d157ebad91cbba00d6b9525e00e5
JamesT1992 <james.trinder@nationwide.co.uk>
2020-11-02 14:08:33 +0000

    Updates

--- scripts/sonarqube.sh
+++ scripts/sonarqube.sh
@@ -1,7 +1,7 @@
 #! /binbash
 
-sonar-scanner \
+echo "sonar-scanner \
   -Dsonar.projectKey=first-project \
   -Dsonar.sources=. \
   -Dsonar.host.url=http://sonarqube:9000 \
-  -Dsonar.login=13b6723b2827dbd4ef52210e3dbae2cefa10a1a7
+  -Dsonar.login=13b6723b2827dbd4ef52210e3dbae2cefa10a1a7"
