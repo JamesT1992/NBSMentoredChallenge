693e671318652aa991f7118d8bd2532819b742f2
JamesT1992 <james.trinder@nationwide.co.uk>
2020-11-02 16:16:45 +0000

    Speak

--- scripts/sonarqube.sh
+++ scripts/sonarqube.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
-
+echo"
 sonar-scanner \
   -Dsonar.projectKey=first-project \
   -Dsonar.sources=. \
   -Dsonar.host.url=http://localhost:9000 \
   -Dsonar.login=13b6723b2827dbd4ef52210e3dbae2cefa10a1a7
+"
